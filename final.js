39819896f3b1f8fbd18e314f612dc376cb6db7b5
mentionslit <bradvanlerberghe@gmail.com>
2020-12-17 10:26:33 -0600

    Removed Google Analytics from js

--- final.js
+++ final.js
@@ -23,19 +23,3 @@ return res.end();
 });
 }).listen(PORT);
 console.log("Server Listening on assigned PORT...");
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-<!-- Google Analytics -->
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-XXXXX-Y', 'auto');
-ga('send', 'pageview');
-</script>
-<!-- End Google Analytics -->
